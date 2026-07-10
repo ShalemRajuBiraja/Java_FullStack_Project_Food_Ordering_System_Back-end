cbb76bc88df6af17aecba8d95fbac86e5dd0b631
Shalem Raju <shalemrajubiraja7@gmail.com>
2026-07-10 16:01:00 +0530

    Initial spring boot backend project set up

---
+++ src/test/java/com/shalemraju/food_ordering_website/FoodOrderingWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shalemraju.food_ordering_website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodOrderingWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
