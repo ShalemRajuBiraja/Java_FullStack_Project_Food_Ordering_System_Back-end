d7a55a21f0db7741c6126b15c5407208ac7edfff
Shalem Raju <shalemrajubiraja7@gmail.com>
2026-07-26 16:40:00 +0530

    Modified the front-end origin

--- src/main/java/com/shalemraju/food_ordering_website/configuration/CorsConfig.java
+++ src/main/java/com/shalemraju/food_ordering_website/configuration/CorsConfig.java
@@ -19,7 +19,7 @@ public class CorsConfig {
                 registry.addMapping("/**")
                         .allowedOrigins(
                                 "http://localhost:5173",
-                                "https://java-full-stack-project-food-orderi.vercel.app")
+                                "https://shalemrajubiraja.github.io/Java_FullStack_Project_Food_Ordering_System_Front-end")
                         .allowedMethods(
                                 "GET",
                                 "POST",
